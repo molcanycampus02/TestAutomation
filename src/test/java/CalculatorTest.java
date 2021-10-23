72eb564374525767fcef082804eea237b25849c5
KarinM <s44787@edu.campus02.at>
2021-10-23 12:11:44 +0200

    Test added

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void subtract() {
+    }
+
+    @Test
+    void multiply() {
+    }
+
+    @Test
+    void divide() {
+    }
+
+    @Test
+    void modulo() {
+    }
+
+    @Test
+    void sum() {
+    }
+}
